b8157deebb290d649e37fc9a16574e6704c6daf8
Vladislav Lewinn <vlewin@suse.de>
2014-03-05 16:43:29 +0100

    push secret_token initilaizer to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,14 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+SimpleSliderDemo::Application.config.secret_token = 'e8c1bc15ffde01d7dcd7ee2fa77bd8aed5989cc5ea6a06f98ecebab247601a521d621c88f7df775478f78909c6d6dfca4af4d1013c6ad157fe3ed58ffa492d4f'
+SimpleSliderDemo::Application.config.secret_key_base = 'b3748869ec3f5c1716dd561e64ce8d558733f50bfeb0cd1718028469ff015bc4861925021a7e15dd49fe7df051510990a001087a0f8563370340601e19496bf2'
