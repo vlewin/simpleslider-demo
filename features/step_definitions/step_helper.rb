0cf8801990d663a6018326fb2b2a05031e423bfb
Vladislav Lewinn <vlewin@suse.de>
2014-03-05 11:19:45 +0100

    don't open a browser if test failed

--- features/step_definitions/step_helper.rb
+++ features/step_definitions/step_helper.rb
@@ -1,7 +1,7 @@
-After do |scenario|
-  if scenario.status == :failed && !page.current_url.blank? && Capybara.current_driver != :selenium
-    puts "\033[36mINFO: Opening page in browser, serving static assets from: #{Capybara.asset_host}\033[0m\n\n"
-    save_and_open_page
-  end
-end
+# After do |scenario|
+#   if scenario.status == :failed && !page.current_url.blank? && Capybara.current_driver != :selenium
+#     puts "\033[36mINFO: Opening page in browser, serving static assets from: #{Capybara.asset_host}\033[0m\n\n"
+#     save_and_open_page
+#   end
+# end
 
